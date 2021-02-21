b5c05a175e4330aa1678a62e1d826f1521eb66a9
VaibhavPrajapati <vaibhavprajapati1@gmail.com>
2021-02-21 07:40:19 +0000

    changed mvn.groovy

--- vars/build.groovy
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env groovy
-def call(String path, String phase) {
-    sh '${path} ${phase}'
-}
--- vars/mvn.groovy
+++ vars/mvn.groovy
@@ -1,4 +1,4 @@
 #!/usr/bin/env groovy
 def call(String path, String phase) {
-    sh '${path} ${phase}'
+    sh "${path} ${phase}
 }
